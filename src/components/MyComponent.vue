27b60bb6e04b2c750822261320af8764629ac897
Piyanuch999 <piyanuch1213141500@gmail.com>
2022-03-13 13:54:10 +0700

    first commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="helloText">
+Hello by Pradit
+</div>
+</template>
+<script>
+export default {
+name: 'MyComponent'
+}
+</script>
+
+<style>
+#helloText {
+color: #178bff;
+}
+</style>
